2767538c923687a8f336549d9fcf27b9c22b4a5a
Swethaa14 <155937832+Swethaa14@users.noreply.github.com>
2024-10-22 08:09:44 +0530

    Add files via upload

---
+++ 3.1 Identical element.py
@@ -0,0 +1,16 @@
+lst = []
+
+print("Input: []")
+print("Expected Output:", lst)
+
+lst = [1]
+print("Input: [1]")
+print("Expected Output:", lst)
+
+lst = [7, 7, 7, 7]
+print("Input: [7, 7, 7, 7]")
+print("Expected Output:", lst)
+
+lst = [-5, -1, -3, -2, -4]
+print("Input: [-5, -1, -3, -2, -4]")
+print("Expected Output:", sorted(lst))
